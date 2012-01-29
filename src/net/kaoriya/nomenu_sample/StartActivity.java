b3ca324f3990230a2ef874f5e27858f64f79f27a
MURAOKA Taro <koron.kaoriya@gmail.com>
2012-01-29 11:43:14 +0900

    first import, skeleton of the project

---
+++ src/net/kaoriya/nomenu_sample/StartActivity.java
@@ -0,0 +1,15 @@
+package net.kaoriya.nomenu_sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
