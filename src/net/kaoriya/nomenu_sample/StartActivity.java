98cbf7a8501e5ba85affebecfff8359af54bac95
MURAOKA Taro <koron.kaoriya@gmail.com>
2012-01-29 12:11:01 +0900

    show some items in ActionBar

--- src/net/kaoriya/nomenu_sample/StartActivity.java
+++ src/net/kaoriya/nomenu_sample/StartActivity.java
@@ -2,6 +2,8 @@ package net.kaoriya.nomenu_sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class StartActivity extends Activity
 {
@@ -12,4 +14,13 @@ public class StartActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.start_activity, menu);
+        return true;
+    }
+
 }
